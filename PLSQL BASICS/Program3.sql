658a64825ce29e678f58807930cd5767b9c60d6b
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2024-08-27 00:52:09 +0530

    Update Program3.sql

--- PLSQL BASICS/Program3.sql
+++ PLSQL BASICS/Program3.sql
@@ -7,21 +7,22 @@ b number;
 c number;
 
 BEGIN
-a := &a;
-b := &b;
-c := &c;
+  a := &a;
+  b := &b;
+  c := &c;
+  
+  IF a > b and a > c then
+        DBMS_OUTPUT.PUT_LINE(a ||' is greator');
+  
+  ELSE IF b > c and b > c then
+      DBMS_OUTPUT.PUT_LINE(b ||' is greator');
+  
+  ELSE
+      DBMS_OUTPUT.PUT_LINE(c ||' is greater');
+  
+  END IF;
+  
+  
 
-IF a > b and a > c then
-      DBMS_OUTPUT.PUT_LINE(a ||' is greator');
-
-ELSE IF b > c and b > c then
-    DBMS_OUTPUT.PUT_LINE(b ||' is greator');
-
-ELSE
-    DBMS_OUTPUT.PUT_LINE(c ||' is greater');
-
-ENDIF;
-
-DBMS_OUTPUT.PUT_LINE('END');
 END;
 /
