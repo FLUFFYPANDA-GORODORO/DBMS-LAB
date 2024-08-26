f680128f7213b6a1126c5365b1ec754929ce8e70
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2024-08-27 00:54:03 +0530

    Update Program3.sql

--- PLSQL BASICS/Program3.sql
+++ PLSQL BASICS/Program3.sql
@@ -2,27 +2,25 @@ SET SERVEROUT ON
 SET VERIFY OFF
   
 DECLARE
-a number;
-b number;
-c number;
+    a number;
+    b number;
+    c number;
 
 BEGIN
-  a := &a;
-  b := &b;
-  c := &c;
+    a := &a;
+    b := &b;
+    c := &c;
+    
+    IF a > b and a > c then
+          DBMS_OUTPUT.PUT_LINE(a ||' is greator');
+    
+    ELSE IF b > c and b > c then
+        DBMS_OUTPUT.PUT_LINE(b ||' is greator');
+    
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(c ||' is greater');
+    
+    ENDIF;
   
-  IF a > b and a > c then
-        DBMS_OUTPUT.PUT_LINE(a ||' is greator');
-  
-  ELSE IF b > c and b > c then
-      DBMS_OUTPUT.PUT_LINE(b ||' is greator');
-  
-  ELSE
-      DBMS_OUTPUT.PUT_LINE(c ||' is greater');
-  
-  END IF;
-  
-  
-
 END;
 /
