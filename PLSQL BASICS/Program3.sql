6981423d8669d8c53259220dcc5599c7105e2eeb
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2024-08-27 18:17:50 +0530

    IF_ELSE

--- PLSQL BASICS/Program3.sql
+++ PLSQL BASICS/Program3.sql
@@ -4,22 +4,20 @@ SET VERIFY OFF
 DECLARE
     a number;
     b number;
-    
-
 BEGIN
     a := &a;
     b := &b;
 
-    
     IF a>b then
         DBMS_OUTPUT.PUT_LINE(a ||' is greator');
     ELSE
-        DBMS_OUTPUT.PUT_LINE(b ||' is greater');
-        
+        DBMS_OUTPUT.PUT_LINE(b ||' is greater');   
     END IF;
 
 END;
 /
+
+-- OUTPUT
 
 -- Enter value for a:3 
 -- Enter value for b:5
