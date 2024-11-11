f5a63e690af82a10ba12ff2cb6fdd1556d653654
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2024-11-11 17:43:06 +0530

    Assignment_5_in_MySQL.sql

--- Assignment_5_in_MySQL.sql
+++ Assignment_5_in_MySQL.sql
@@ -1,24 +1,80 @@
-CREATE table stud_marks(name varchar(20), total_marks int);
-CREATE table result(RollNo int, name varchar(20), class varchar(30));
+create table studentMarks(rollNo int , name varchar(25),marks int);
+
+create table result(rollNo int,name varchar(25),grade varchar(25));
+
+delimiter //
+create function insertstudent(r int,n varchar(25),m int)
+returns varchar(25)
+deterministic
+begin
+insert into studentMarks values(r,n,m);
+return "Successful";
+end;
+//
+
+select insertstudent(201,"Gauarv",1450);
+select insertstudent(202,"Sumedh",1350);
+select insertstudent(203,"Karan",1280);
+//
 
 
- delimiter //
-  create procedure grade( IN roll_new int, IN name varchar(20), IN marks int)
-   begin
-    if(marks<=1500  and marks>=990) then
-     insert into result values(roll_new, name, 'Distinction');
-    elseif( marks <=989 and marks>=900) then
-    insert into result values (roll_new, name, 'First-Class');
-    elseif(marks<=899 and marks>825) then
-    insert into result values(roll_new, name, 'Higher Second Class');
-    elseif(marks<=825) then
-    insert into result values(roll_new, name, 'Fail');
-    end if;
-    insert into stud_marks values(name, marks);
-     end;
-     //
+--  | insertstudent(201,"Gauarv",1450) |
+-- +----------------------------------+
+-- | Successful                       |
+-- +----------------------------------+
+-- 1 row in set (0.02 sec)
+
+-- +----------------------------------+
+-- | insertstudent(202,"Sumedh",1350) |
+-- +----------------------------------+
+-- | Successful                       |
+-- +----------------------------------+
+-- 1 row in set (0.02 sec)
+
+-- +---------------------------------+
+-- | insertstudent(203,"Karan",1280) |
+-- +---------------------------------+
+-- | Successful                      |
+
+create procedure calculateGrade(IN roll INT,OUT class VARCHAR(25))
+begin 
+declare c varchar(25);
+declare m int;
+declare n varchar(100);
+select marks into m from studentMarks where rollNo = roll;
+select name into n from studentMarks where rollNo = roll;
+
+if m>=990 and m<=1500 then
+set c='Distinction';
+elseif m>=900 and m<=989 then
+set c='First Class';
+elseif m>=825 and m<=899 then
+set c='Higher Second Class';
+else
+set c='Fail';
+end if;
+insert into result values(roll,n,c);
+set class=c;
+End;
+//
+
+call calculateGrade(201,@class)
+//
 
 
---Create a function to input and call the grade procedure--
+select * from studentMarks;
+//
 
---Run the Function--
+-- | rollNo | name   | marks |
+-- +--------+--------+-------+
+-- |    201 | Gauarv |  1450 |
+-- |    202 | Sumedh |  1350 |
+-- |    203 | Karan  |  1280 |
+
+
+select * from result;
+
+-- | rollNo | name   | grade       |
+-- +--------+--------+-------------+
+-- |    201 | Gauarv | Distinction |
+-- +--------+--------+-------------+
