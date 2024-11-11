ebb6d9bdbcf1283db28335b6a2f51fd655527fb2
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2024-11-11 14:55:07 +0530

    Assignment_4_In_MySQL.sql

--- Assignment_4_In_MySQL.sql
+++ Assignment_4_In_MySQL.sql
@@ -1,104 +1,49 @@
-CREATE DATABASE IF NOT EXISTS Library;
-use Library;
-CREATE TABLE IF NOT EXISTS Borrower(RollNo int(20), Name varchar(20), DOI date, NOB varchar(20), Status varchar(20));
+create table borrower(rollNo int,name varchar(20),issueDate date,bookName varchar(20),status varchar(20));
 
-DESC Borrower;
+create table fine(rollNo int , returnDate date,Amount int);
 
- /*+--------+-------------+------+-----+---------+-------+
-| Field  | Type        | Null | Key | Default | Extra |
-+--------+-------------+------+-----+---------+-------+
-| RollNo | int         | YES  |     | NULL    |       |
-| Name   | varchar(20) | YES  |     | NULL    |       |
-| DOI    | date        | YES  |     | NULL    |       |
-| NOB    | varchar(20) | YES  |     | NULL    |       |
-| Status | varchar(20) | YES  |     | NULL    |       |
-+--------+-------------+------+-----+---------+-------+*/
+ insert into borrower values(201,"Gaurav",'2024-10-21',"CNS","Issued");
+ insert into borrower values(202,"Sumedh",'2024-10-10',"DBMS","Issued");
+ insert into borrower values(203,"Karan",'2024-10-14',"OS","Issued");
 
-INSERT INTO Borrower VALUES(101, 'Ram', '2022-09-20', 'DBMS', 'Issued');
+Delimiter //
 
-INSERT INTO Borrower VALUES(102, 'Varun', '2022-08-25', 'CN', 'Issued');
+create procedure CalculateFine(In Roll_No int , Name varchar(20))
 
-INSERT INTO Borrower VALUES(103, 'Rani', '2022-08-01', 'SPOS', 'Issued');
+Begin
+    
+     declare Issue_Date date;
+     declare Day_Difference int;
+     declare Fine_Amount int;
+     declare Exit Handler for 1062 select "Fine already paid";
+    
+     select issueDate into Issue_Date from borrower where rollNO = Roll_No and name = Name;
+     select DateDiff(Curdate(),Issue_Date) into Day_Difference;
+    
+     if(Day_Difference >=15 and Day_Difference <= 30 ) then
+     set Fine_amount = Day_Difference*5;
+     insert into fine values(Roll_No,Curdate(),Fine_amount);
+    
+     elseif(Day_Difference>30) then
+     set Fine_Amount = Day_Difference*50;
+     insert into fine values(Roll_No,Curdate(),Fine_Amount);
+    
+     End if;
 
-SELECT * FROM Borrower;
+     update borrower set status = "Returned" where rollNO = Roll_No and name = Name;
 
-/*
-+--------+-------+------------+------+--------+
-| RollNo | Name  | DOI        | NOB  | Status |
-+--------+-------+------------+------+--------+
-|    101 | Ram   | 2022-09-10 | DBMS | Issued |
-|    102 | Varun | 2022-08-25 | CN   | Issued |
-|    103 | Rani  | 2022-08-01 | SPOS | Issued |
-+--------+-------+------------+------+--------+
-*/
-
-CREATE TABLE IF NOT EXISTS Fine(RollNo int(20), Date date, Amount int(50));
-
-DESC Fine;
-
-/*
-+--------+------+------+-----+---------+-------+
-| Field  | Type | Null | Key | Default | Extra |
-+--------+------+------+-----+---------+-------+
-| RollNo | int  | YES  |     | NULL    |       |
-| Date   | date | YES  |     | NULL    |       |
-| Amount | int  | YES  |     | NULL    |       |
-+--------+------+------+-----+---------+-------+
-*/
-
-delimiter //
-CREATE PROCEDURE B(roll_new int, book_name varchar(20))
-BEGIN
-DECLARE x integer;
-DECLARE continue handler for NOT FOUND
-BEGIN
-SELECT 'NOT FOUND';
-END;
-SELECT datediff(curdate(), DOI) into x from Borrower where RollNo=roll_new;
-if(x>15 && x<30)
-then
-INSERT INTO Fine VALUES(roll_new, curdate(),(x*5));
-end if;
-if(x>30)
-then 
-INSERT INTO Fine VALUES(roll_new, curdate(), (x*50));
-end if;
-UPDATE Borrower SET Status='returned' where RollNo=roll_new;
-END;
+End;
 //
 
-CALL B(101, 'Ram');
+call CalculateFine(201,"Gaurav");
+call CalculateFine(202,"Sumedh");
+call CalculateFine(203,"Karan");
 //
 
-CALL B(102, 'Varun');
-//
+select * from fine;
 
-CALL B(103, 'Rani');
-//
-
-SELECT * FROM Fine;
-//
-
-/*
-+--------+------------+--------+
-| RollNo | Date       | Amount |
-+--------+------------+--------+
-|    101 | 2024-09-18 |  36950 |
-|    102 | 2024-09-18 |  37750 |
-|    103 | 2024-09-18 |  38950 |
-+--------+------------+--------+
-*/
-
-SELECT * FROM Borrower;
-//
-/*
-+--------+-------+------------+------+----------+
-| RollNo | Name  | DOI        | NOB  | Status   |
-+--------+-------+------------+------+----------+
-|    101 | Ram   | 2022-09-10 | DBMS | returned |
-|    102 | Varun | 2022-08-25 | CN   | returned |
-|    103 | Rani  | 2022-08-01 | SPOS | returned |
-+--------+-------+------------+------+----------+
-*/
-
-/*HelloWorld*/
+-- | rollNo | returnDate | Amount |
+-- +--------+------------+--------+
+-- |    201 | 2024-11-11 |    105 |
+-- |    203 | 2024-11-11 |    140 |
+-- |    202 | 2024-11-11 |   1600 |
